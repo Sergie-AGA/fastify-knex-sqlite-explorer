f80f54b11382154f172f0c7902e141e6cc1bea60
SergieAGA2 <sergiearruda@gmail.com>
2023-08-09 23:10:33 +0100

    Prisma setup

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 import { transactionsRoutes } from './routes/transactions'
 import { pollsRoutes } from './routes/polls'
 import cookie from '@fastify/cookie'
@@ -6,6 +7,8 @@ import cookie from '@fastify/cookie'
 export const app = fastify()
 
 app.register(cookie)
+
+const prisma = new PrismaClient()
 
 // app.addHook('preHandler', async (req, res) => {
 // Here I can assign a middleware that runs for any request
