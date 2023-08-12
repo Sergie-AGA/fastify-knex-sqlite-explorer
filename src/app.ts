f4c0dbf52140cc047365b71bd53d9e1780cfb04b
SergieAGA2 <sergiearruda@gmail.com>
2023-08-12 18:57:58 +0100

    Docker integration and initial routing and further schema

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
 import { transactionsRoutes } from './routes/transactions'
 import { pollsRoutes } from './routes/polls'
 import cookie from '@fastify/cookie'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
 app.register(cookie)
-
-const prisma = new PrismaClient()
 
 // app.addHook('preHandler', async (req, res) => {
 // Here I can assign a middleware that runs for any request
@@ -19,4 +18,23 @@ app.register(transactionsRoutes, {
 })
 app.register(pollsRoutes, {
   prefix: 'polls',
+})
+app.post('/users', async (req, res) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return res.status(201).send()
 })
