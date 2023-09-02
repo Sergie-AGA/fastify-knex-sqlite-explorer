ed11a78641ad1b75aeec52446315cadbb6b32916
SergieAGA2 <sergiearruda@gmail.com>
2023-09-02 22:56:52 +0100

    Finish e2e test setup and register test

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,16 @@
 import 'dotenv/config'
 import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 function generateDatabaseURL(schema: string) {
-  if (!process.env.DATABASE_URL) {
-    throw new Error('Please provide a DATABASE_URL environment variable')
+  if (!process.env.POSTGRES_URL) {
+    throw new Error('Please provide a POSTGRES_URL environment variable')
   }
-
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(process.env.POSTGRES_URL)
 
   url.searchParams.set('schema', schema)
 
@@ -20,11 +23,17 @@ export default <Environment>{
     const schema = randomUUID()
     const databaseURL = generateDatabaseURL(schema)
 
-    process.env.DATABASE_URL = databaseURL
+    process.env.POSTGRES_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('teardown')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
       },
     }
   },
