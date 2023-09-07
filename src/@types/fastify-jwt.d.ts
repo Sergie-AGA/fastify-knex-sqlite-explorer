bb476c821467fc4a41c704a66218ab8098976ae5
SergieAGA2 <sergiearruda@gmail.com>
2023-09-07 22:48:27 +0100

    Middleware to handle token

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -3,12 +3,15 @@ import '@fastify/jwt'
 
 declare module '@fastify/jwt' {
   export interface FastifyJWT {
-    // payload: { id: number } // payload type is used for signing and verifying. Not using it now
+    // payload: {
+    //   id: number // payload type is used for signing and verifying.
+    // }
     user: {
       // id: number
       // name: string
       // age: number
       sub: string
+      role: 'ADMIN' | 'MEMBER'
     } // user type is return type of `request.user` object
   }
 }
--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -1,57 +1,41 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { z } from 'zod'
-import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
-import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
-export async function authenticate(req: FastifyRequest, res: FastifyReply) {
-  // Sanitise data
-  const authenticateBodySchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
+export async function refresh(req: FastifyRequest, res: FastifyReply) {
+  await req.jwtVerify({ onlyCookie: true })
 
-  const { email, password } = authenticateBodySchema.parse(req.body)
-
-  try {
-    const authenticateUseCase = makeAuthenticateUseCase()
-
-    const { user } = await authenticateUseCase.execute({ email, password })
-
-    const token = await res.jwtSign(
-      {},
-      {
-        sign: {
-          sub: user.id,
-        },
+  const { role } = req.user
+  const token = await res.jwtSign(
+    {
+      role,
+    },
+    {
+      sign: {
+        sub: req.user.sub,
       },
-    )
+    },
+  )
 
-    const refreshToken = await res.jwtSign(
-      {},
-      {
-        sign: {
-          sub: user.id,
-          expiresIn: '7d',
-        },
+  const refreshToken = await res.jwtSign(
+    {
+      role,
+    },
+    {
+      sign: {
+        sub: req.user.sub,
+        expiresIn: '7d',
       },
-    )
+    },
+  )
 
-    return res
-      .setCookie('refreshToken', refreshToken, {
-        path: '/',
-        secure: true,
-        sameSite: true,
-        httpOnly: true,
-      })
-      .status(200)
-      .send({
-        token,
-      })
-  } catch (err) {
-    if (err instanceof InvalidCredentialsError) {
-      return res.status(400).send()
-    }
-
-    throw err
-  }
+  return res
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({
+      token,
+    })
 }
